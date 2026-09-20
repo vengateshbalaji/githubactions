1a08d6e3ac0d88f08c55a1f237c23a557860443b
vengateshbalaji <vengatesh.balaji@gmail.com>
2026-09-20 19:05:39 +0530

    Configure Azure provider and resource group outputs
    Add Azure provider and resource group data outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,19 @@
+provider "azurerm" {
+  features {}
+}
 
+data "azurerm_resource_group" "example" {
+  name = "rg_sb_eastus_133366_1_178991092992"
+}
+
+output "resource_group_name" {
+  value = data.azurerm_resource_group.example.name
+}
+
+output "resource_group_id" {
+  value = data.azurerm_resource_group.example.id
+}
+
+output "resource_group_location" {
+  value = data.azurerm_resource_group.example.location
+}
